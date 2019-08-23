76940dc5290df3925d45db75cd08d3be21d761a4
Edgar Teixeira <edgar.tx@outlook.com>
2019-08-22 22:13:39 -0300

    Added skeleton for Transactions page

--- frontend/src/components/main.jsx
+++ frontend/src/components/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Home from "./home/home";
 import Wallet from "./wallet/wallet";
+import Transactions from "./transactions/transactions";
 import { Switch, Route } from "react-router-dom";
 
 const Main = () => {
@@ -9,6 +10,7 @@ const Main = () => {
             <Switch>
                 <Route path="/" component={Home} exact />
                 <Route path="/wallet" component={Wallet} />
+                <Route path="/transactions" component={Transactions} />
             </Switch>
         </main>
     );
